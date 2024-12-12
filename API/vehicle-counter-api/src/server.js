7ff61df4c1b64a3c327ba1c4609a6be2b9265c9e
brrrka <149936049+brrrka@users.noreply.github.com>
2024-12-12 19:22:19 +0700

    Update server.js

--- API/vehicle-counter-api/src/server.js
+++ API/vehicle-counter-api/src/server.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
